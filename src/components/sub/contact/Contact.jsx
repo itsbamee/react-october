8e19eedf7317870b3df481a8488165f3cabe5457
itsbamee <jan9239@naver.com>
2023-11-11 15:07:25 +0900

    지도 중첩되는 문제와 리사이즈시 지도 위치 중심으로 안오는 오류 해결

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -5,6 +5,7 @@ import './Contact.scss';
 export default function Contact() {
 	const { kakao } = window;
 	const mapFrame = useRef(null);
+	const mapInstance = useRef(null);
 	const [Index, setIndex] = useState(0);
 
 	const info = useRef([
@@ -40,10 +41,23 @@ export default function Contact() {
 		),
 	});
 
+	const setCenter = () => {
+		mapInstance.current.setCenter(info.current[Index].latlng);
+	};
+
 	useEffect(() => {
-		const map = new kakao.maps.Map(mapFrame.current, { center: info.current[Index].latlng });
-		marker.setMap(map);
+		mapFrame.current.innerHTML = '';
+		mapInstance.current = new kakao.maps.Map(mapFrame.current, {
+			center: info.current[Index].latlng,
+		});
+		marker.setMap(mapInstance.current);
+
+		window.addEventListener('resize', setCenter);
 	}, [Index]);
+
+	useEffect(() => {
+		return () => window.removeEventListener('resize', setCenter);
+	}, []);
 
 	return (
 		<Layout title={'Contact us'}>
