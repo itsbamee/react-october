1b1dbe3fc2077ba9961c2c9932dfe2fc825d9f42
itsbamee <jan9239@naver.com>
2023-11-12 14:34:34 +0900

    로드뷰 상태에서 위치 초기화 적용, 교통정보버튼 숨김

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -45,7 +45,16 @@ export default function Contact() {
 		),
 	});
 
-	const setCenter = () => mapInstance.current.setCenter(info.current[Index].latlng);
+	const roadView = () => {
+		new kakao.maps.RoadviewClient().getNearestPanoId(info.current[Index].latlng, 50, (id) => {
+			new kakao.maps.Roadview(viewFrame.current).setPanoId(id, info.current[Index].latlng);
+		});
+	};
+
+	const setCenter = () => {
+		mapInstance.current.setCenter(info.current[Index].latlng);
+		roadView();
+	};
 
 	useEffect(() => {
 		mapFrame.current.innerHTML = '';
@@ -60,14 +69,9 @@ export default function Contact() {
 		mapInstance.current.setZoomable(false);
 		marker.setMap(mapInstance.current);
 
-		//로드뷰 설정
-		//두번째 수치값 50은 (m단위) 마커 위치로부터 로드뷰가 출력될 수 있는 가장 가까운 거리의 범위지정
-		new kakao.maps.RoadviewClient().getNearestPanoId(info.current[Index].latlng, 50, (id) => {
-			new kakao.maps.Roadview(viewFrame.current).setPanoId(id, info.current[Index].latlng);
-		});
-
 		setTraffic(false);
 		setView(false);
+		roadView(false);
 
 		window.addEventListener('resize', setCenter);
 	}, [Index]);
@@ -99,9 +103,11 @@ export default function Contact() {
 			</ul>
 
 			<button onClick={setCenter}>위치 초기화</button>
-			<button onClick={() => setTraffic(!Traffic)}>
-				{Traffic ? '교통정보 끄기' : '교통정보 보기'}
-			</button>
+			{!View && (
+				<button onClick={() => setTraffic(!Traffic)}>
+					{Traffic ? '교통정보 끄기' : '교통정보 보기'}
+				</button>
+			)}
 			<button onClick={() => setView(!View)}>{View ? '지도보기' : '로드뷰 보기'}</button>
 		</Layout>
 	);
