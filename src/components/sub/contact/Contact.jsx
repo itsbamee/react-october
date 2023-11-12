c257339c885afd0b98c5a48c78d7a5ce38747fc7
itsbamee <jan9239@naver.com>
2023-11-12 15:20:08 +0900

    메일 전송시 필수입력사항 체크 및 전송성공유무 경고창 출력 및 인풋요소 초기화

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -49,13 +49,21 @@ export default function Contact() {
 
 	const sendEmail = (e) => {
 		e.preventDefault();
+		const [user, email] = form.current.querySelectorAll('input');
+		const txtArea = form.current.querySelector('textarea');
+
+		if (!user.value || !email.value || !txtArea.value)
+			return alert('이름, 답장받을 메일주소, 문의내용을 모두 입력하세요.');
 
 		emailjs.sendForm('service_gvv9bxs', 'template_asgcq1j', form.current, 'Wh1QLfOMqbY_nHpsl').then(
 			(result) => {
-				console.log(result.text);
+				alert('문의내용이 성공적으로 전달되었습니다.');
+				[user, email, txtArea].forEach((el) => (el.value = ''));
 			},
 			(error) => {
-				console.log(error.text);
+				alert('문의내용 전송에 실패했습니다.');
+				console.error(error);
+				[user, email, txtArea].forEach((el) => (el.value = ''));
 			}
 		);
 	};
@@ -115,6 +123,7 @@ export default function Contact() {
 					<label>Message</label>
 					{/* message : 템플릿에서 textarea 부분 문의메세지 변수명 */}
 					<textarea name='message' />
+					<input type='reset' value={'Reset'} />
 					<input type='submit' value='Send' />
 				</form>
 			</div>
