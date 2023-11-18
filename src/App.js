bb80b35539fec93d92aa19d0601dce92357bb082
itsbamee <jan9239@naver.com>
2023-11-18 13:57:57 +0900

    메인,서브에 따라 header에 클래스명 분기처리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Footer from './components/common/footer/Footer';
 import Header from './components/common/header/Header';
-import Visual from './components/main/visual/Visual';
 import Contact from './components/sub/contact/Contact';
 import Department from './components/sub/department/Department';
 import Community from './components/sub/community/Community';
@@ -10,23 +9,28 @@ import Detail from './components/sub/youtube/Detail';
 import Youtube from './components/sub/youtube/Youtube';
 import { useMedia } from './hooks/useMedia';
 import './styles/Global.scss';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import MainWrap from './components/main/mainWrap/MainWrap';
 
 function App() {
 	return (
 		<main className={useMedia()}>
-			<Header />
-			<Route exact path='/'>
-				<MainWrap />
-			</Route>
+			{/* Switch : 중첩된 라우터로 복수개의 동일한 컴포넌트가 연결될 때 처음 연결되는 라우터만 실행되고 나머지는 무시 */}
+			<Switch>
+				<Route exact path='/'>
+					<Header isMain={true} />
+					<MainWrap />
+				</Route>
+				<Route path='/'>
+					<Header isMain={false} />
+				</Route>
+			</Switch>
 			<Route path='/department' component={Department} />
 			<Route path='/community' component={Community} />
 			<Route path='/gallery' component={Gallery} />
 			<Route path='/youtube' component={Youtube} />
 			<Route path='/members' component={Members} />
 			<Route path='/contact' component={Contact} />
-			{/* 특정 URL 라우터 설정시 유튜브의 고유 아이디값을 params로 전달 */}
 			<Route path='/detail/:id' component={Detail} />
 			<Footer />
 		</main>
