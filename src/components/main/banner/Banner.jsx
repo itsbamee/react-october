9b271d598a3bfbc2ac673b7a8edab8106d324865
itsbamee <jan9239@naver.com>
2023-11-18 13:45:34 +0900

    메인전용 컴포넌트들을 그룹화시키는 MainWrap 컴포넌트 추가 및 연결

---
+++ src/components/main/banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from './Banner.scss';
+
+function Banner() {
+	return (
+		<section>
+			<h1>Banner</h1>
+		</section>
+	);
+}
+
+export default Banner;
---
+++ src/components/main/info/Info.jsx
@@ -0,0 +1,11 @@
+import React from './Info.scss';
+
+function Info() {
+	return (
+		<section>
+			<h1>Flickr</h1>
+		</section>
+	);
+}
+
+export default Info;
