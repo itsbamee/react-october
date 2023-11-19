6af5d678e82704c1c8c4770e5227ea3be56e8e12
itsbamee <jan9239@naver.com>
2023-11-19 12:45:12 +0900

    섹션의 전체 갯수값 state로 변경

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -1,20 +1,19 @@
 import './Btns.scss';
-import { useEffect, useRef } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import Anime from '../../../asset/anime.js';
 
 function Btns() {
-	//각각의 활성화순번, 버튼 그룹요소, 섹션 그룹요소 담길 참조객체 생성
-	const num = useRef(0);
+	//section의 전체 갯수가 담길 값을 참조객체에서 state로 변경
+	//Num값 변경시 컴포넌트가 재랜더링되어야 버튼이 생기므로 state처리
+	const [Num, setNum] = useState(0);
 	const secs = useRef(null);
 	const btns = useRef(null);
 
-	//추후에 컴포넌트 마운트시 윈도우 스크롤 이벤트 될 때 동작(연결될)되는 함수
+	//컴포넌트 마운트시 윈도우 스크롤이벤트에 연결될 함수
 	const activation = () => {
 		const scroll = window.scrollY;
-
 		secs.current.forEach((el, idx) => {
 			if (scroll >= el.offsetTop - window.innerHeight / 2) {
-				//순수배열로 반환 (노드js로 되서 어쩌구)
 				Array.from(btns.current.children).forEach((btn) => btn.classList.remove('on'));
 				btns.current.children[idx].classList.add('on');
 			}
@@ -22,16 +21,20 @@ function Btns() {
 	};
 
 	const handleClick = (idx) => {
-		new Anime(window, { scroll: secs.current[idx].offsetTop }, { duration: 500 });
+		new Anime(
+			window,
+			{ scroll: secs.current[idx].offsetTop },
+			{ duration: 500, easeType: 'ease1' }
+		);
 	};
 
 	//컴포넌트 마운트시
 	useEffect(() => {
 		//빈 참조객체에 버튼과 section요소 담아줌
 		secs.current = document.querySelectorAll('.myScroll');
-		num.current = secs.current.length;
+		setNum(secs.current.length);
 
-		//window scroll 이벤트에 activation 함수 연결
+		//window scroll이벤트에 activation함수 연결
 		window.addEventListener('scroll', activation);
 
 		return () => {
@@ -41,7 +44,7 @@ function Btns() {
 
 	return (
 		<ul className='btns' ref={btns}>
-			{Array(num.current)
+			{Array(Num)
 				.fill()
 				.map((_, idx) => {
 					return (
