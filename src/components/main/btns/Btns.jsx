1c4cab5b08ce54a82ea31ea2b49d5b6f584177ae
itsbamee <jan9239@naver.com>
2023-11-25 13:01:23 +0900

    useRef 추가설명

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -2,6 +2,13 @@ import './Btns.scss';
 import { useRef, useEffect, useState } from 'react';
 import Anime from '../../../asset/anime.js';
 
+// document.querySelector vs useRef(DOM) (참고로 둘다 realDOM을 제어)
+// document.querySelector : 이미 이전 렌더링 사이클에서 돔으로 변경된 신뢰할 수 없는 예전 돔
+// useRef : 똑같이 realDOM을 담긴 하지만 앞으로 렌더링이 될 신뢰할 수 있는 최신 돔
+// State vs useRef 해당 값의 변경 및 적용 시점
+// state값은 해당 렌더링 사이클에서 값이 변경되는 것은 맞지만 실제 그 값이 적용되는 시점은 다음번 렌더링 사이클
+// useRef값은 해당 렌더링 사이클에서 값도 변경되고 바로 반용도 됨
+
 function Btns() {
 	const [Num, setNum] = useState(0);
 	const secs = useRef(null);
@@ -42,9 +49,7 @@ function Btns() {
 	}, []);
 
 	//Num state 변경시 activation 호출
-	useEffect(() => {
-		activation();
-	}, [Num]);
+	useEffect(activation, [Num]);
 
 	return (
 		<ul className='btns' ref={btns}>
