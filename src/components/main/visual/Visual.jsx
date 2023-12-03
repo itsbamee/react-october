f204ac662df3af2bc7fd9447e1badb9e6653c50c
itsbamee <jan9239@naver.com>
2023-12-03 14:21:45 +0900

    swiper 일반 스타일 모션처리, 썸네일 레터박스 없애기, 반응형처리

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -2,23 +2,25 @@ import './Visual.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper';
 import 'swiper/css';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 export default function Visual() {
 	const [Index, setIndex] = useState(0);
 
 	const [SlideData, setSlideData] = useState([]);
-	const path = useRef(process.env.PUBLIC_URL);
 
-	const fetchData = async () => {
-		const data = await fetch(`${path.current}/DB/department.json`);
+	const fetchYoutube = async () => {
+		const api_key = process.env.REACT_APP_YOUTUBE_KEY;
+		const pid = process.env.REACT_APP_PLAYLIST;
+		const num = 10;
+		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+		const data = await fetch(baseURL);
 		const json = await data.json();
-		console.log(json.members);
-		setSlideData(json.members);
+		setSlideData(json.items);
 	};
 
 	useEffect(() => {
-		fetchData();
+		fetchYoutube();
 	}, []);
 
 	return (
@@ -29,28 +31,47 @@ export default function Visual() {
 						if (idx >= 5) return null;
 						return (
 							<li key={idx} className={idx === Index ? 'on' : ''}>
-								{tit.name}
+								<h3>{tit.snippet.title}</h3>
+								<span>
+									<em>View Detail</em>
+								</span>
 							</li>
 						);
 					})}
 				</ul>
 			</div>
+
 			<Swiper
 				modules={[Autoplay]}
-				spaceBetween={50}
-				slidesPerView={3}
+				spaceBetween={0}
+				slidesPerView={1}
 				loop={true}
 				centeredSlides={true}
-				autoplay={{ delay: 1500, disableOnInteraction: true }}
+				autoplay={{ delay: 2000, disableOnInteraction: true }}
 				onSlideChange={(el) => setIndex(el.realIndex)}
+				breakpoints={{
+					1000: {
+						slidesPerView: 2,
+						spaceBetween: 50,
+					},
+					1400: {
+						slidesPerView: 3,
+						spaceBetween: 50,
+					},
+				}}
 			>
 				{SlideData.map((data, idx) => {
 					if (idx >= 5) return null;
 					return (
 						<SwiperSlide key={idx}>
 							<div className='pic'>
-								<img src={`${path.current}/img/${data.pic}`} alt={data.name} />
-								<img src={`${path.current}/img/${data.pic}`} alt={data.name} />
+								<p>
+									<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+								</p>
+
+								<p>
+									<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+								</p>
 							</div>
 						</SwiperSlide>
 					);
@@ -61,8 +82,8 @@ export default function Visual() {
 }
 
 /*
-	swiper 연결 순서
+	swiper연결 순서
 	1. npm i swiper@8 설치 (현재 버전은 9버전이지만 react가 17버전이므로 8버전대 설치)
-	2. swiper 가이드문서 예시코드 그대로 붙여넣기
-	3. .swiper > .swiper-wrapper > .swiper-slide 해당구조를 파악해서 styling
+	2. swiper가이드문서 예시코드 그대로 붙여넣기
+	3. .swiper> .swiper-wrapper > .swiper-slide 해당구조를 파악해서 styling
 */
