a96c33f9aac29178da146184229d6994eddbf786
itsbamme <jan9239@naver.com>
2024-01-06 17:57:22 +0900

    전역데이터를 footer 컴포넌트에서 자유롭게 호출

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import { configureStore } from 'redux';
+import { createStore } from 'redux';
 import reducers from './reducer';
 
 //store공간을 생성한 뒤, 리듀서가 전달해주는 데이터를 저장
-const store = configureStore(reducers);
+const store = createStore(reducers);
 export default store;
